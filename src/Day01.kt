d50765900e90b716cab54430662c4c893a5f24cc
Emmanuel John <emmanuel.john@twinhealth.com>
2022-12-01 09:21:55 -0600

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,43 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun getCalories(input: List<String>) = buildList {
+        var currentSum = 0L
+        for ((idx, value) in input.withIndex()) {
+            if (value.isEmpty()) {
+                add(currentSum)
+                currentSum = 0
+            } else {
+                currentSum += value.toLong()
+                if (idx == input.size - 1) {
+                    add(currentSum)
+                }
+            }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        val calories = getCalories(input)
+        return calories.asSequence().sorted().drop(calories.size - 1).sum()
+    }
+
+    fun part2(input: List<String>): Long {
+        val calories = getCalories(input)
+        return calories.asSequence().sorted().drop(calories.size - 3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("inputs/Day01_test")
+    println(part1(testInput))
+    println(part2(testInput))
 
-    val input = readInput("Day01")
+    println("----- Real input -------")
+    val input = readInput("inputs/Day01")
     println(part1(input))
     println(part2(input))
 }
+
+// Output:
+// 24000
+// 45000
+// ----- Real input -------
+// 67633
+// 199628
